f8449fc7a6d4b52a3ccd5c85d9164ed7f55f3ffd
lum8rjack <4932619+lum8rjack@users.noreply.github.com>
2023-01-12 21:14:27 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # caddy-maxmind-asn
-Caddy v2 module to filter requests based on ASN
+Caddy v2 module to filter requests based on ASN.
+
+## Installation
+
+You can download a Caddy build with this plugin inside directly from the [official Caddy page](https://caddyserver.com/download).
+
+If you prefer, you can build Caddy by yourself by [installing xcaddy](https://github.com/caddyserver/xcaddy) and running:
+```
+xcaddy build --with github.com/lum8rjack/caddy-maxmind-asn
+```
+
+## Requirements 
+
+To be able to use this module you will need to have a MaxMind GeoLite2 database, that can be downloaded for free
+by creating an account. More information about this are available on the
+[official website](https://dev.maxmind.com/geoip/geoip2/geolite2/).
+
+You will specifically need the `GeoLite2-ASN.mmdb` file.
+
+## Usage
+
+You can use this module as a matcher to allow or block a set of Autonomous System Organization. The ASOs are non case-sensitive and will match as long as the ASN contains the provided matcher.
+
+Example: Providing 'allow_asos amazon' will match on AMAZON and AMAZON-12
+
+
+### Caddyfile
+
+1. Allow access to the website only from ASNs associated with Amazon:
+```
+test.example.org {
+  @myasn {
+    maxmind_asn {
+      db_path "/usr/share/MaxMind/GeoLite2-ASN.mmdb"
+      allow_asos amazon
+    }
+  }
+
+   file_server @myasn {
+     root /var/www/html
+   }
+}
+
+```
+
+2. Deny access to the website from ASNs associated with Amazon:
+```
+test.example.org {
+  @myasn {
+    maxmind_geolocation {
+      db_path "/usr/share/MaxMind/GeoLite2-ASN.mmdb"
+      deny_asos amazon
+    }
+  }
+
+   file_server @myasn {
+     root /var/www/html
+   }
+}
+
+```
---
+++ main.go
@@ -0,0 +1,164 @@
+package caddy_maxmind_asn
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/oschwald/maxminddb-golang"
+	"go.uber.org/zap"
+)
+
+// Interface guards
+var (
+	_ caddy.Module             = (*MaxmindASN)(nil)
+	_ caddyhttp.RequestMatcher = (*MaxmindASN)(nil)
+	_ caddy.Provisioner        = (*MaxmindASN)(nil)
+	_ caddy.CleanerUpper       = (*MaxmindASN)(nil)
+	_ caddyfile.Unmarshaler    = (*MaxmindASN)(nil)
+)
+
+type Record struct {
+	ASN int    `maxminddb:"autonomous_system_number"`
+	ASO string `maxminddb:"autonomous_system_organization"`
+}
+
+func init() {
+	caddy.RegisterModule(MaxmindASN{})
+}
+
+// Allows to filter requests based on source IP ASO.
+type MaxmindASN struct {
+	// The path of the MaxMind GeoLite2-ASN.mmd file
+	DbPath string `json:"db_path"`
+
+	// A list of
+	AllowASOs []string `json:"allow_asos"`
+	DenyASOs  []string `json:"deny_asos"`
+
+	// MaxMind database reader
+	dbInst *maxminddb.Reader
+	logger *zap.Logger
+}
+
+func (m *MaxmindASN) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	current := 0
+	for d.Next() {
+		for nesting := d.Nesting(); d.NextBlock(nesting); {
+			switch d.Val() {
+			case "db_path":
+				current = 1
+			case "allow_asos":
+				current = 2
+			case "deny_asos":
+				current = 3
+			default:
+				switch current {
+				case 1:
+					m.DbPath = d.Val()
+					current = 0
+				case 2:
+					m.AllowASOs = append(m.AllowASOs, d.Val())
+				case 3:
+					m.DenyASOs = append(m.DenyASOs, d.Val())
+				default:
+					return fmt.Errorf("unexpected config parameter %s", d.Val())
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func (MaxmindASN) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.matchers.maxmind_asn",
+		New: func() caddy.Module { return new(MaxmindASN) },
+	}
+}
+
+func (m *MaxmindASN) Provision(ctx caddy.Context) error {
+	var err error
+	m.logger = ctx.Logger(m)
+	m.dbInst, err = maxminddb.Open(m.DbPath)
+	if err != nil {
+		return fmt.Errorf("cannot open database file %s: %v", m.DbPath, err)
+	}
+	return nil
+}
+
+func (m *MaxmindASN) Cleanup() error {
+	if m.dbInst != nil {
+		return m.dbInst.Close()
+	}
+	return nil
+}
+
+func (m *MaxmindASN) checkAllowed(asoresult string) bool {
+	if asoresult == "" {
+		return true
+	}
+	if len(m.DenyASOs) > 0 {
+		for _, i := range m.DenyASOs {
+			if i == asoresult {
+				return false
+			}
+		}
+		return true
+	}
+	if len(m.AllowASOs) > 0 {
+		for _, i := range m.AllowASOs {
+			if i == asoresult {
+				return true
+			}
+		}
+		return false
+	}
+	return true
+}
+
+func (m *MaxmindASN) Match(r *http.Request) bool {
+	// If both the allow and deny fields are empty, let the request pass
+	if len(m.AllowASOs) < 1 && len(m.DenyASOs) < 1 {
+		return false
+	}
+
+	// Get the remote address from the web request
+	remoteIp, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		m.logger.Warn("cannot split IP address", zap.String("address", r.RemoteAddr), zap.Error(err))
+	}
+
+	// Convert the IP string to net.IP
+	addr := net.ParseIP(remoteIp)
+	if addr == nil {
+		m.logger.Warn("cannot parse IP address", zap.String("address", r.RemoteAddr))
+		return false
+	}
+
+	// Get the record from the database
+	var record Record
+	err = m.dbInst.Lookup(addr, &record)
+	if err != nil {
+		m.logger.Warn("cannot lookup IP address", zap.String("address", r.RemoteAddr), zap.Error(err))
+		return false
+	}
+
+	m.logger.Debug(
+		"Detected MaxMind data",
+		zap.String("ip", r.RemoteAddr),
+		zap.Int("autonomous_system_number", record.ASN),
+		zap.String("autonomous_system_organization", record.ASO),
+	)
+
+	// Check if the IP against the allowed/denied ASOs
+	if !m.checkAllowed(record.ASO) {
+		m.logger.Debug("ASO not allowed", zap.String("autonomous_system_organization", record.ASO))
+		return false
+	}
+
+	return true
+}
